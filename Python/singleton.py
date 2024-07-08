7b9ae6a21063452b0735930957dc7bff8107d25b
nnnocturnn <mediandrey@gmail.com>
2024-07-08 23:16:50 +0100

    feat: add singleton gof

---
+++ Python/singleton.py
@@ -0,0 +1,18 @@
+
+class SingletonBaseClass(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonBaseClass, cls).\
+                __call__(*args, **kwargs)
+        return cls._instances[cls]
+    
+
+class Singleton(metaclass=SingletonBaseClass):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+    
