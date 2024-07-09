7d1672485d5296863123fb0653d2d172a02a2a5e
nnnocturnn <mediandrey@gmail.com>
2024-07-09 21:33:42 +0100

    feat: add absfactory & factory method

---
+++ Python/creational/absfactory.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+class Button(ABC):
+    @abstractmethod
+    def paint(self):
+        pass
+
+class DarkButton(Button):
+    def paint(self):
+        return "Dark Button"
+    
+class LightButton(Button):
+    def paint(self):
+        return "Light Button"
+    
+class BlueButton(Button):
+    def paint(self):
+        return "Blue Button"
+    
+class ButtonFactory(ABC):
+    @abstractmethod
+    def create_button(self):
+        pass
+
+class DarkButtonFactory(ButtonFactory):
+    def create_button(self):
+        return DarkButton()
+    
+class LightButtonFactory(ButtonFactory):
+    def create_button(self):
+        return LightButton()
+
+class BlueButtonFactory(ButtonFactory):
+    def create_button(self):
+        return BlueButton()
+    
+class Application:
+    def __init__(self, factory):
+        self.factory = factory.create_button()
+        
+    def paint(self):
+        return self.factory.paint()
+    
+
+if __name__ == "__main__":
+    theme = "dark"
+    factory = DarkButtonFactory() if theme == "dark" else LightButtonFactory()
+    app = Application(factory)
+    app.paint()
---
+++ Python/creational/factory_method.py
@@ -0,0 +1,41 @@
+from enum import Enum
+
+
+class ApiType(Enum):
+    INSTAGRAM = 1
+    FACEBOOK = 2
+    TWITTER = 3
+
+class Api:
+    def __init__(self, flags):
+        self.__flags = flags
+
+    def get_flags(self):
+        return self.__flags
+    
+class InstagramApi(Api):
+    def __init__(self):
+        super().__init__("-f -s -d")
+
+class FacebookApi(Api):
+    def __init__(self):
+        super().__init__("-f -s -d -t")
+
+class TwitterApi(Api):
+    def __init__(self):
+        super().__init__("-f -s -d -t -w")
+
+
+def create_api(api_type: ApiType) -> Api:
+    api_factory = {
+        ApiType.INSTAGRAM: InstagramApi,
+        ApiType.FACEBOOK: FacebookApi,
+        ApiType.TWITTER: TwitterApi
+    }
+    return api_factory[api_type]()
+
+
+if __name__ == "__main__":
+    for i in ApiType:
+        api = create_api(i)
+        print(api.get_flags())
